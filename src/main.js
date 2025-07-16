c3a496702a8069e30c9bae8d1c86b72cad1ab1c8
Franklin Otero <franklin.otero6@gmail.com>
2025-07-15 20:36:04 -0600

    updates

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { BreakingBadApp } from './breakingbad/breakingbad-app';
+import { UsersApp } from './users/users-app';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -21,6 +22,7 @@ document.querySelector('#app').innerHTML = `
 
 const element = document.querySelector('.card');
 
-BreakingBadApp(element);
+//BreakingBadApp(element);
+UsersApp(element);
 
 
---
+++ src/users/store/users-store.js
@@ -0,0 +1,50 @@
+import { User } from "../models/user";
+import { loadUsersByPage } from "../use-cases/load-users-by-page"
+
+
+const state = {
+    currentPage: 0,
+    users: [],
+}
+
+const loadNextPage = async() => {
+    const users = await loadUsersByPage( state.currentPage + 1 );
+    if (users.length === 0) return;
+
+    state.currentPage += 1;
+    state.users = users;
+}
+
+const loadPreviousPage = async() => {
+    const users = await loadUsersByPage( state.currentPage - 1);
+    if( state.currentPage === 1) return;
+
+    state.currentPage -= 1;
+    state.users = users;
+}
+const onUserChanged = async() => {
+    throw new Error('No implementado')
+}
+const reloadPage = async() => {
+    throw new Error('No implementado')
+}
+
+export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChanged,
+    reloadPage,
+
+    /**
+     * 
+     * @returns {User[]}
+     */
+    getUsers: () => [...state.users],
+
+    /**
+     * 
+     * @returns {Number}
+     */
+    getCurrentPage: () => state.currentPage,
+
+}
